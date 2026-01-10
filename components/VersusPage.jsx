9baf4f2c97979d233998288112c63eb9c96d1d65
ecurrencyhodler <ecurrencyhodler@gmail.com>
2026-01-09 22:44:34 -0500

    Added rules to create versus page layout

--- components/VersusPage.jsx
+++ components/VersusPage.jsx
@@ -427,7 +427,18 @@ export default function VersusPage({
           <header>
             <h1>Satdoku</h1>
           </header>
-          <h2 className="versus-create-heading">Create Versus Game</h2>
+          <div className="versus-rules">
+            <h3 className="versus-rules-title">Versus Rules</h3>
+            <ol className="versus-rules-list">
+              <li>Type in your name below</li>
+              <li>Select a difficulty</li>
+              <li>Invite a challenger by sharing a link</li>
+              <li>Both players press start to play</li>
+              <li>Player with more points at the end of the gamewins</li>
+              <li>Buy a life with bitcoin if you run out</li>
+            </ol>
+          </div>
+          <h2 className="versus-create-heading"></h2>
           <div className="difficulty-selection">
             <label className="versus-form-label">Your Name:</label>
             <input
@@ -437,6 +448,7 @@ export default function VersusPage({
               maxLength={20}
               className="name-input versus-name-input"
               placeholder="Player 1"
+              autoFocus
             />
             <label className="versus-form-label">Select Difficulty:</label>
             <div className="difficulty-buttons">
